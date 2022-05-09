3afdc329ba5f0826b89332bae3ee01f4faf4f9ad
Stefan Mühlbauer <github@stmb-mail.de>
2022-05-09 16:56:15 +0200

    Init Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"iobadaptertest adapter settings": {
+		"en": "Adapter settings for iobadaptertest",
+		"de": "Adaptereinstellungen für iobadaptertest",
+		"ru": "Настройки адаптера для iobadaptertest",
+		"pt": "Configurações do adaptador para iobadaptertest",
+		"nl": "Adapterinstellingen voor iobadaptertest",
+		"fr": "Paramètres d'adaptateur pour iobadaptertest",
+		"it": "Impostazioni dell'adattatore per iobadaptertest",
+		"es": "Ajustes del adaptador para iobadaptertest",
+		"pl": "Ustawienia adaptera dla iobadaptertest",
+		"zh-cn": "iobadaptertest的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
